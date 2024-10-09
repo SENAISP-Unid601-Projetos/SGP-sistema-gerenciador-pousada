3af534df457fb9c2655d7a42497b833c66329cdf
LeandroGuidelli <leandrosoaresguidelli@gmail.com>
2024-10-08 22:45:09 -0300

    Projeto atualizado

--- ProjetoNovo/ProjetoFinal/app.py
+++ ProjetoNovo/ProjetoFinal/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 from datetime import datetime
 
 app = Flask(__name__)
