7b15d96868786fddc04d66680adf5ad52fa08fbd
LeandroGuidelli <leandrosoaresguidelli@gmail.com>
2024-10-22 21:00:38 -0300

    ta funcionando a reserva

--- ProjetoFinal/app.py
+++ ProjetoFinal/app.py
@@ -60,41 +60,15 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-@app.route('/addData', methods=['POST'])
-def submit_data():
-    # Coletando os dados do formulário
-    nome = request.form.get('nome')
-    email = request.form.get('email')
-    quarto = request.form.get('quarto')
-    checkin = request.form.get('checkin')  # Ex: 25/01/2005
-    checkout = request.form.get('checkout')  # Ex: 30/01/2005
+@app.route('/meu_perfil')
+def meu_perfil():
+    if 'usuario_id' not in session:
+        return redirect(url_for('login'))  # Redireciona se não estiver logado
+    
+    usuario_id = session['usuario_id']
+    reservas_usuario = reservas_db.search(Usuario.email == session['email'])  # Busca as reservas do usuário
 
-    # Convertendo as datas para o formato esperado
-    try:
-        checkin_formatado = datetime.strptime(checkin, '%d/%m/%Y').strftime('%Y-%m-%d')
-        checkout_formatado = datetime.strptime(checkout, '%d/%m/%Y').strftime('%Y-%m-%d')
-    except ValueError:
-        return jsonify({'error': 'Erro: Formato de data inválido. Utilize dia/mês/ano!'}), 400
-
-    # Verificando se os dados estão preenchidos
-    if nome and email and checkin_formatado and checkout_formatado and quarto:
-        
-        # Verifica se há um conflito de reservas para o mesmo quarto
-        if verificar_conflito(quarto, checkin_formatado, checkout_formatado):
-            return jsonify({'error': 'Erro: Já existe uma reserva para este cômodo nas datas selecionadas!'}), 400
-
-        # Salvando os dados no banco TinyDB
-        reservas_db.insert({
-            'nome': nome,
-            'email': email,
-            'quarto': quarto,
-            'checkin': checkin_formatado,
-            'checkout': checkout_formatado,
-            'data_registro': datetime.now().strftime('%d/%m/%Y às %H:%M:%S')  # Formato DD/MM/AAAA
-        })
-        return jsonify({'success': 'Reserva efetuada com sucesso!'}), 200  # Redireciona de volta para a página inicial
-
-    return jsonify({'error': 'Erro: Todos os campos são obrigatórios!'}), 400
+    return render_template('meu_perfil.html', user=session, reservas=reservas_usuario)
 
 @app.route('/reserva')
 def reserva():
@@ -124,40 +98,34 @@ def chale():
 def cabana():
     return render_template('cabana.html')
 
-@app.route('/meu_perfil')
-def meu_perfil():
-    if 'usuario_id' not in session:
-        return redirect(url_for('login'))  # Redireciona se não estiver logado
-    
-    usuario_id = session['usuario_id']
-    reservas_usuario = reservas_db.search(Usuario.email == session['email'])  # Busca as reservas do usuário
+@app.route('/addData', methods=['POST'])
+def submit_data():
+    # Coletando os dados do formulário
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    quarto = request.form.get('quarto')
+    checkin = request.form.get('checkin')  
+    checkout = request.form.get('checkout')  
 
-    return render_template('meu_perfil.html', user=session, reservas=reservas_usuario)
+    # Verificando se os dados estão preenchidos
+    if nome and email and checkin and checkout and quarto:
+        
+        # Verifica se há um conflito de reservas para o mesmo quarto
+        if verificar_conflito(quarto, checkin, checkout):
+            return jsonify({'error': 'Erro: Já existe uma reserva para este cômodo nas datas selecionadas!'}), 400
 
-@app.route('/admin', methods=['GET', 'POST'])
-def admin():
-    reservas = []
-    if request.method == 'POST':
-        if 'ver_todas' in request.form:  # Verifica se o botão de "Ver todas" foi clicado
-            reservas = reservas_db.all()  # Retorna todas as reservas do banco de dados
-        else:
-            termo_pesquisa = request.form.get('termo').lower()  # Convertendo o termo pesquisado para minúsculas
-            Quarto = Query()
-            # Busca insensível a maiúsculas/minúsculas usando regex
-            reservas = reservas_db.search(
-                (Quarto.quarto.matches(termo_pesquisa, flags=re.IGNORECASE)) |
-                (Quarto.nome.matches(termo_pesquisa, flags=re.IGNORECASE)) |
-                (Quarto.email.matches(termo_pesquisa, flags=re.IGNORECASE)) |
-                (Quarto.checkin.matches(termo_pesquisa, flags=re.IGNORECASE)) |
-                (Quarto.checkout.matches(termo_pesquisa, flags=re.IGNORECASE)) 
-            )
+        # Salvando os dados no banco TinyDB (as datas já estão no formato correto)
+        reservas_db.insert({
+            'nome': nome,
+            'email': email,
+            'quarto': quarto,
+            'checkin': checkin,  # Formato AAAA-MM-DD
+            'checkout': checkout,  # Formato AAAA-MM-DD
+            'data_registro': datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Formato ISO
+        })
+        return jsonify({'success': 'Reserva efetuada com sucesso!'}), 200  # Redireciona de volta para a página inicial
 
-    # Formata as datas antes de passar para o template
-    for reserva in reservas:
-        reserva['checkin'] = datetime.strptime(reserva['checkin'], '%Y-%m-%d').strftime('%d/%m/%Y')
-        reserva['checkout'] = datetime.strptime(reserva['checkout'], '%Y-%m-%d').strftime('%d/%m/%Y')
-
-    return render_template('admin.html', reservas=reservas)
+    return jsonify({'error': 'Erro: Todos os campos são obrigatórios!'}), 400
 
 # Função para obter todas as datas reservadas
 def obter_datas_reservadas():
@@ -169,7 +137,7 @@ def obter_datas_reservadas():
         checkout = datetime.strptime(reserva['checkout'], '%Y-%m-%d')
 
         while checkin <= checkout:
-            datas_reservadas.append(checkin.strftime('%d/%m/%Y'))  # Formato DD/MM/AAAA
+            datas_reservadas.append(checkin.strftime('%d/%m/%Y'))   # Formato DD/MM/AAAA
             checkin += timedelta(days=1)
 
     return datas_reservadas
@@ -194,6 +162,33 @@ def verificar_conflito(quarto, checkin, checkout):
             return True  # Conflito encontrado
 
     return False  # Sem conflitos
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    reservas = []
+    if request.method == 'POST':
+        if 'ver_todas' in request.form:  # Verifica se o botão de "Ver todas" foi clicado
+            reservas = reservas_db.all()  # Retorna todas as reservas do banco de dados
+        else:
+            termo_pesquisa = request.form.get('termo').lower()  # Convertendo o termo pesquisado para minúsculas
+            Quarto = Query()
+            # Busca insensível a maiúsculas/minúsculas usando regex
+            reservas = reservas_db.search(
+                (Quarto.quarto.matches(termo_pesquisa, flags=re.IGNORECASE)) |
+                (Quarto.nome.matches(termo_pesquisa, flags=re.IGNORECASE)) |
+                (Quarto.email.matches(termo_pesquisa, flags=re.IGNORECASE)) |
+                (Quarto.checkin.matches(termo_pesquisa, flags=re.IGNORECASE)) |
+                (Quarto.checkout.matches(termo_pesquisa, flags=re.IGNORECASE)) 
+            )
+
+    # Formata as datas antes de passar para o template
+    for reserva in reservas:
+        reserva['checkin'] = datetime.strptime(reserva['checkin'], '%Y-%m-%d').strftime('%d/%m/%Y')
+        reserva['checkout'] = datetime.strptime(reserva['checkout'], '%Y-%m-%d').strftime('%d/%m/%Y')
+        reserva['data_registro'] = datetime.strptime(reserva['data_registro'], '%Y-%m-%d %H:%M:%S').strftime('%d/%m/%Y às %H:%M:%S')
+
+    return render_template('admin.html', reservas=reservas)
+
 
 # Inicia o servidor
 if __name__ == '__main__':
